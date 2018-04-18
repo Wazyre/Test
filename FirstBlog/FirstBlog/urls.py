e03c91d88cb9b2bfb20f2bc5bf16bf1b8a709bc0
Aziz Alabduljalil <azalabduljalil@gmail.com>
2018-04-18 13:59:39 -0700

    Build a skeleton for a website

--- FirstBlog/FirstBlog/urls.py
+++ FirstBlog/FirstBlog/urls.py
@@ -15,7 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Use include() to add paths from the blog application
+
+
+urlpatterns += [
+    path('blog/', include('blog.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+
+urlpatterns += [
+    path('/', RedirectView.as_view(url='/blog/')),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
